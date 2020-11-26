6c931f25c76ac2f3fc6a35ff712b2d6c88bbd7cf
elmoatas <58230241+elmoatas@users.noreply.github.com>
2020-11-27 00:32:20 +0100

    les 1 verplichte opdracht
    xaml code aangepast
    visibility van de buttons aangepast

--- les 1/ToDoApplicatie/MainWindow.xaml.cs
+++ les 1/ToDoApplicatie/MainWindow.xaml.cs
@@ -9,51 +9,76 @@ namespace ToDoApplicatie
     /// </summary>
     public partial class MainWindow : Window
     {
-
-
         public MainWindow()
         {
             InitializeComponent();
-
+            ChangeVisibility(Visibility.Hidden);
+            AddButton.IsEnabled = false;
         }
-
-        private void addButton_Click(object sender, RoutedEventArgs e)
+        private void ChangeVisibility(Visibility a)
         {
-
+            loadButton.Visibility = a;
+            deleteButton.Visibility = a;
+            saveButton.Visibility = a;
+            upButton.Visibility = a;
+            downButton.Visibility = a;
+        }
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
             //Toevoegen van todo in listbox
             ListBoxItem todo = new ListBoxItem();
-            todo.Content = inputToDoTextbox.Text;
             if (!toDoListbox.Items.Contains(inputToDoTextbox.Text) && inputToDoTextbox.Text != "")
             {
+
                 toDoListbox.Items.Add(inputToDoTextbox.Text);
                 inputToDoTextbox.Text = "";
+                AddButton.IsEnabled = true;
             }
             else
             {
-                AddButton.IsEnabled = false;
+
                 inputToDoTextbox.Text = "";
+                AddButton.IsEnabled = false;
             }
         }
 
         private void deleteButton_Click(object sender, RoutedEventArgs e)
         {
+            if (toDoListbox.SelectedItem != null)
+            {
+                toDoListbox.Items.Remove(toDoListbox.SelectedItem);
 
-            toDoListbox.Items.Remove(toDoListbox.SelectedItem);
-
-        }
-        private void verify()
-        {
-
+            }
 
         }
+
         private void inputToDoTextbox_KeyUp(object sender, KeyEventArgs e)
         {
             //via stackoverflow:You can handle the keydown event of your TextBox control
             if (e.Key == Key.Enter)
             {
-                addButton_Click(sender, e);
+                AddButton_Click(sender, e);
             }
         }
+
+
+        private void upButton_Click(object sender, RoutedEventArgs e)
+        {
+            toDoListbox.SelectedIndex = toDoListbox.SelectedIndex - 1;
+        }
+
+        private void inputToDoTextbox_KeyDown(object sender, KeyEventArgs e)
+        {
+            AddButton.IsEnabled = true;
+            ChangeVisibility(Visibility.Hidden);
+        }
+
+        private void toDoListbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ChangeVisibility(Visibility.Visible);
+        }
+
+   
     }
 }
 
