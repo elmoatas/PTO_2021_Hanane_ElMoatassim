e0625bdefef049a0d12af1c9751ef61fd9ab73b2
elmoatas <58230241+elmoatas@users.noreply.github.com>
2020-12-10 11:41:41 +0100

    Les 4: DEMO boekenapplicatie begrijpen

---
+++ LES 4/Demo_boekenapplicatie/Books.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Demo_boekenapplicatie
+{
+    class Books
+    {
+        // Private members / class attributes / class variables
+        private string title;
+        private string author;
+        private int totalNumberOfPages;
+        private int currentPageReading;
+        private string reader;
+
+
+        // Constructor 
+        public Books(string title, string author, int totalNumberOfPages)
+        {
+            this.title = title;
+            this.author = author;
+            this.totalNumberOfPages = totalNumberOfPages;
+            this.currentPageReading = 0;
+        }
+
+        public Books(string title, string author, int totalNumberOfPages, string reader)
+        {
+            this.title = title;
+            this.author = author;
+            this.totalNumberOfPages = totalNumberOfPages;
+            this.currentPageReading = 0;
+            this.reader = reader;
+        }
+
+        // Properties / getters & settersop _(om private variabelen uit te lezen en aan te passen) 
+        //read only property
+        public string Title
+        {
+            get { return title; }
+        }
+
+        public string Author
+        {
+            get { return author; }
+        }
+
+        public int TotalNumberOfPages
+        {
+            get { return totalNumberOfPages; }
+        }
+         //full acces
+        public string Reader
+        {
+            get { return reader; }
+            set { reader = value; }
+        }
+
+
+
+
+        // Methodes (public & private) ( doen iets met een object meestal aan de hand van instantievariabelen)
+        public string NextPage()
+        {
+            if (currentPageReading < totalNumberOfPages) 
+            {
+                currentPageReading++;
+                return $"Pagina {currentPageReading} van de {totalNumberOfPages} in het boek {title}";
+            }
+
+            return "Einde van het boek!";
+        }
+
+        public void StartReading()
+        {
+            do
+            {
+                Console.WriteLine(NextPage());
+            } while (Console.ReadKey().KeyChar != 'x');
+
+        }
+
+        public string ToString()
+        {
+            return $"{title} - {author}";
+        }
+
+    }
+}
---
+++ LES 4/Demo_boekenapplicatie/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Demo_boekenapplicatie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Books myFirstBook = new Books("De Kleine Prins", "Blabla", 10); //classe books: public Books(string title, string author, int totalNumberOfPages)
+            Books mySecondBook = new Books("Harry Potter", "Blabla", 10);
+
+            List<Books> books = new List<Books>();
+            books.Add(myFirstBook);
+            books.Add(mySecondBook);
+            myFirstBook.Reader = "Matthias";
+
+            char action;
+
+            do
+            {
+                Console.WriteLine("Welk boek wil je lezen?");
+                action = Console.ReadKey().KeyChar;
+                switch (action)
+                {
+                    case '1':
+                        books[0].StartReading();
+                        break;
+                    case '2':
+                        books[1].StartReading();
+                        break;
+                }
+
+            } while (action != 'x');
+        }
+    }
+}
