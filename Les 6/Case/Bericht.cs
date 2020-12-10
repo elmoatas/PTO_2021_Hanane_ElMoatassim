3014eb061c4faa665efded46bbb90cb1fc12fdbc
elmoatas <58230241+elmoatas@users.noreply.github.com>
2020-12-10 18:50:08 +0100

    klasse bericht maken

---
+++ Les 6/Case/Bericht.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Case
+{
+    class Bericht
+    {
+        // Private members / class attributes / class variables
+        private string sender;
+        private string content;
+        private DateTime date;
+        private string receiver;
+        // Constructor 
+        public Bericht()
+        {
+
+        }
+        // Properties / getters & setters
+        public string Sender
+        {
+            get { return sender; }
+        }
+        public string Content
+        {
+            get { return content; }
+        }
+        public DateTime Date
+        {
+            get { return date; }
+        }
+        public string Receiver
+        {
+            get { return receiver; }
+        }
+        // Methodes (public & private)
+        public string Display()
+        {
+            string display="";
+            return display;
+        }
+        public void Send()
+        {
+
+
+        }
+    }
+}
