63514ee32df9a665ba8c19516c412ba1ab9a84be
elmoatas <58230241+elmoatas@users.noreply.github.com>
2020-12-11 17:22:32 +0100

    create verichtingen ( opdracht van de les)

---
+++ Les 6/Spaarrekening/Verrichting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spaarrekening
+{
+    class Verrichting
+    {
+        DateTime datumEnTijd;
+        double bedrag;
+        string mededeling;
+    }
+}
