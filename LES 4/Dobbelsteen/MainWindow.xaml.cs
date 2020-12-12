5562d2d96d7fe56fd0829dd5e2c3b314adb394e8
elmoatas <58230241+elmoatas@users.noreply.github.com>
2020-12-13 00:15:14 +0100

    xaml code van todoapp2

---
+++ LES 4/Dobbelsteen/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+
+namespace Dobbelsteen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Dice Dice1 = new Dice("dobbelsteen 1");
+        public MainWindow()
+
+        {
+            InitializeComponent();
+            Random rnd = new Random();
+            rnd.Next(0, 6);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Dice1.Roll();
+            imagedice.Source = Dice1.Image;
+        }
+    }
+}
--- LES 4/TO_DO_part2/MainWindow.xaml.cs
+++ LES 4/TO_DO_part2/MainWindow.xaml.cs
@@ -23,6 +23,90 @@ namespace TO_DO_part2
         public MainWindow()
         {
             InitializeComponent();
+            EnableButtons();
         }
+        private void EnableButtons()
+        {
+        
+            downButton.IsEnabled = toDoListbox.SelectedIndex + 1 < toDoListbox.Items.Count;
+            upButton.IsEnabled = toDoListbox.SelectedIndex > 0;
+            fulldownButton.IsEnabled = toDoListbox.SelectedIndex + 1 < toDoListbox.Items.Count;
+            fullUpButton.IsEnabled = toDoListbox.SelectedIndex > 0;
+            AddButton.IsEnabled = inputToDoTextbox.Text != "";
+        }
+
+        private void AddItemToList()
+        {
+            if (!toDoListbox.Items.Contains(inputToDoTextbox.Text) && inputToDoTextbox.Text != "")
+            {
+                toDoListbox.Items.Add(inputToDoTextbox.Text);
+                inputToDoTextbox.Text = "";
+            }
+            else
+            {
+                inputToDoTextbox.Text = "";
+            }
+        }
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Toevoegen van todo in listbox
+            AddItemToList();
+            EnableButtons();
+        }
+        private void InputToDoTextbox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                AddItemToList();
+            }
+            EnableButtons();
+        }
+        private void GoUpOrGoDown(int upOrDown)
+        {
+            int newIndex = toDoListbox.SelectedIndex + upOrDown;
+            object selectedItem = toDoListbox.SelectedItem;
+            toDoListbox.Items.Remove(selectedItem);
+            toDoListbox.Items.Insert(newIndex, selectedItem);
+            toDoListbox.SelectedIndex = newIndex;
+        }
+        private void UpButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (toDoListbox.SelectedIndex >= 0)
+            {
+                GoUpOrGoDown(-1);
+            }
+            EnableButtons();
+        }
+
+        private void DownButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (toDoListbox.SelectedIndex + 1 <= toDoListbox.Items.Count)
+            {
+                GoUpOrGoDown(1);
+            }
+            EnableButtons();
+        }
+        private void FullUpDown(int placementOfItem)
+        {
+            object selectedItem = toDoListbox.SelectedItem;
+            toDoListbox.Items.Remove(selectedItem);
+            toDoListbox.Items.Insert(placementOfItem, selectedItem);
+            toDoListbox.SelectedIndex = placementOfItem;
+            upButton.IsEnabled = false;
+            fullUpButton.IsEnabled = false;
+        }
+        private void fullUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            FullUpDown(0);
+        }
+        private void fulldownButton_Click(object sender, RoutedEventArgs e)
+        {
+            FullUpDown(toDoListbox.Items.Count - 1);
+        }
+        private void ToDoListbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            EnableButtons();
+        }   
+
     }
 }
