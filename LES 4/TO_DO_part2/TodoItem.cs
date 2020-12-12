f52137e799125cbe7b06bdce094e4a20e8f2ff94
elmoatas <58230241+elmoatas@users.noreply.github.com>
2020-12-13 00:38:45 +0100

    classe todoItems gemaakt part 1

---
+++ LES 4/TO_DO_part2/TodoItem.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Media;
+
+namespace TO_DO_part2
+{
+    class TodoItem
+    {
+        private string name;
+        private Brush color;
+        private DateTime duedate;
+        private DateTime startDate;
+        private DateTime finishDate;
+        private string info;
+        //constructor
+        //properties
+        //methodes
+        public string GetName()
+        {
+            string name = "";
+            return name;
+        }
+        public void SetName(string name)
+        {
+
+        }
+        public Brush GetColor()
+        {
+            Brush color = Brushes.Transparent;
+            return color ;
+        }
+        public void SetColor(Brush color)
+        {
+
+        }
+        public DateTime GetStartDate() 
+        {
+            DateTime dueTime = DateTime.Now;
+            return dueTime;
+        }
+        public DateTime GetFinishDate() 
+        {
+            DateTime finishTime = DateTime.Now;
+            return finishTime;
+        }
+        public DateTime GetDueDate()
+        {
+            DateTime dueTime= DateTime.Now;
+            return dueTime;
+        }
+        public void SetDueDate(DateTime dueDate)
+        {
+
+        }
+        public void Start()
+        {
+
+        }
+        public void Stop ()
+        {
+        
+        }
+        public string GetState()
+        {
+            string state="";
+            return state;
+        }
+    
+    }
+}
--- LES 5/DEMO_FILES_AND_FOLDERS/MainWindow.xaml.cs
+++ LES 5/DEMO_FILES_AND_FOLDERS/MainWindow.xaml.cs
@@ -7,23 +7,44 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Text_editor
+namespace DEMO_FILES_AND_FOLDERS
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        public object Folderbrowserdialog { get; private set; }
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private void pathButton_Click(object sender, RoutedEventArgs e)
+        {
+            openFolderBrowserDialog();
+        }
+        private void openFolderBrowserDialog()
+        {
+            
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void filesListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
