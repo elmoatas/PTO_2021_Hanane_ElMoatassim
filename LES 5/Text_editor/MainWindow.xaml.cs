05f24f2ed9e15739e521b63e3bf27651f830ee6f
Hanane <58230241+elmoatas@users.noreply.github.com>
2020-12-29 16:21:41 +0100

    les 5 texteditor

--- LES 5/Text_editor/MainWindow.xaml.cs
+++ LES 5/Text_editor/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Forms;
 using Forms = System.Windows.Forms;
+using MessageBox = System.Windows.Forms.MessageBox;
+
 namespace Text_editor
 {
     /// <summary>
@@ -10,33 +13,96 @@ namespace Text_editor
     public partial class MainWindow : Window
     {
         string selectedPath;
+        string path;
         public MainWindow()
         {
             InitializeComponent();
-
         }
 
-        private void OpenTheFile()
+        private void StreamWriter(string path)
         {
-            FolderBrowserDialog folderBrowseDialog = new FolderBrowserDialog();
 
-            folderBrowseDialog.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            DialogResult dialogResult = folderBrowseDialog.ShowDialog();
 
-            if (dialogResult == Forms.DialogResult.OK)
-            {
-                selectedPath = folderBrowseDialog.SelectedPath;
-                pathLabel.Content = selectedPath;
-            }
+            StreamWriter streamWriter = new StreamWriter(path);
+            streamWriter.WriteLine("Hello World2!");
+            streamWriter.Close();
         }
 
         private void pathButton_Click(object sender, RoutedEventArgs e)
         {
-            OpenTheFile();
+            OpenFolderBrowserDialog();
+        }
+
+        private void OpenFolderBrowserDialog()
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.SelectedPath = Environment.CurrentDirectory;
+            DialogResult dialogResult = folderBrowserDialog.ShowDialog();
+
+            if (dialogResult == Forms.DialogResult.OK)
+            {
+                selectedPath = folderBrowserDialog.SelectedPath;
+                pathLabel.Content = selectedPath;
+                LoadDataFromFolder();
+            }
+            else if (dialogResult == Forms.DialogResult.Cancel)
+            {
+                MessageBox.Show("Geen selectie");
+            }
+        }
+
+        private void LoadDataFromFolder()
+        {
+            filesListBox.Items.Clear();
+            // load data
+            string[] files = Directory.GetFiles(selectedPath);
+
+            foreach (string file in files)
+            {
+                if (file.EndsWith(".txt"))
+                {
+                    filesListBox.Items.Add(file);
+                }
+            }
         }
 
         private void addButton_Click(object sender, RoutedEventArgs e)
         {
+            string fileName = fileNameTextBox.Text;
+            path = Path.Combine(selectedPath, fileName + ".txt");
+            filesListBox.Items.Add(path);
+            fileNameTextBox.Text = "";
+            StreamWriter(path);
+        }
+
+        private void filesListBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            path = filesListBox.SelectedItem.ToString();
+            OpenNewWindow(StreamReader(path));
+        }
+
+        //new window
+        private void OpenNewWindow(string inputText)
+        {
+            Window1 windowTwo = new Window1(inputText);
+            windowTwo.Show();
+        }
+
+        private string StreamReader(string path)
+        {
+            // open streamreader
+            StreamReader streamReader = new StreamReader(path);
+
+            // bestand lijn per lijn inlezen
+            string line = streamReader.ReadLine();
+            while (line != null)
+            {
+                line = streamReader.ReadLine();
+            }
+            return line;
+
+            // sluit streamreader
+            //streamReader.Close();
 
         }
     }
---
+++ LES 5/Text_editor/Window1.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Text_editor
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private string inputText;
+        public string InputText { get => inputText; set => inputText = value; }
+       
+        public Window1(string inputText)
+        {
+            InitializeComponent();
+            this.inputText = inputText;
+            ShowText();
+        }
+        private void ShowText()
+        {
+            inputTextBox.Text = inputText;
+        }
+
+        private void NoteTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                inputTextBox.Text += Environment.NewLine; 
+            }
+        }
+    }
+}
